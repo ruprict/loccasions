feaa4bc043c12508a5e8a483730a70f0e6b79786
Glenn Goodrich <glenn.goodrich@gmail.com>
2011-09-07 10:34:40 -0400

    Added event form and controller method to create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,12 @@ class EventsController < ApplicationController
   def index
     @events = current_user.events
   end
+  
+  def create
+    event = current_user.events.build(params[:event])
+    event.save
+    redirect_to events_path
+  end
+
+
 end
